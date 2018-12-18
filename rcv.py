82bbe366248fa9d0ffc492989b90b14420379519
Ben Fogarty <ben.t.fogarty@gmail.com>
2018-12-17 18:19:37 -0600

    Finished read_and_process_dataset function

--- rcv.py
+++ rcv.py
@@ -2,6 +2,9 @@
 Maine 2018 2nd Congressional District Re-creation
 
 Ben Fogarty
+
+All references to Maine election law from:
+https://www.maine.gov/sos/cec/elec/upcoming/pdf/250c535-2018-230-complete.pdf
 '''
 
 import pandas as pd
@@ -16,6 +19,8 @@ DATA_FILE_LOCS = {'digital1': 'data/NOV18CVRExportFINAL1.xlsx',
 				  'noscan7': 'data/AUXCVRProofedCVR95RepCD2.xlsx',
 				  'digital8': 'data/RepCD2-8final.xlsx'
 				 }
+CHOICE_FIELDS = ['first_choice', 'second_choice', 'third_choice',
+				 'fourth_choice', 'fifth_choice']
 
 def compute_election_results():
 	'''
@@ -53,21 +58,78 @@ def read_and_process_dataset(filepath):
 		df = pd.read_excel(filepath, index_col="Cast Vote Record",
 						   dtypes=col_types)
 		df.rename(col_names, axis=1, inplace=True)
+		df = df.apply(process_overvotes, axis=1)
 
 	except FileNotFoundError:
 		print("No file found at", filepath + "!")
 
 	return df
 
+
 def process_overvotes(ballot):
 	'''
-	Eliminates the overvoted ranking and all subsequent rankings in accordance
-	with Maine election law athttps://www.maine.gov/sos/cec/elec/upcoming/pdf/250c535-2018-230-complete.pdf
-	Section 4.2.B.i.
+	Handles overvotes, undervotes, and duplicate rankings for a single candidate
+	in accordance with Maine election law in Section 4.2.B.
 
 	Inputs:
 		ballot (Pandas series): one ballot from the election
 
 	Returns: Pandas series
 	'''
-	#To be implemented
+	candidates_voted_for = set()
+	for i, col in enumerate(CHOICE_FIELDS):
+		#process overvotes
+		if ballot[col] == 'overvote':
+			ballot = exhaust_beyond(ballot, i)
+			break
+		#process undervotes
+		elif ballot[col] == 'undervote':
+			if i == len(CHOICE_FIELDS) - 1 or \
+			ballot[CHOICE_FIELDS[i + 1]] == 'undervote':
+				ballot = exhaust_beyond(ballot, i)
+				break
+			else:
+				ballot = move_forward_one_choice(ballot, i)
+		#process duplicate rankings
+		if ballot[col] in candidates_voted_for:
+			if i == len(CHOICE_FIELDS) - 1:
+				ballot[CHOICE_FIELDS[i]] == float('nan')
+			else:
+				ballot = move_forward_one_choice(ballot, i)	
+		else:
+			candidates_voted_for.add(ballot[col])
+
+	return ballot
+
+def move_forward_one_choice(ballot, ind):
+	'''
+	Moves all the choices at or after index ind forward by one ranking.
+
+	Inputs:
+		ballot (Pandas series): one ballot from the election
+		ind (int): the index to start the process at
+
+	Returns: Pandas series
+	'''
+	ballot[CHOICE_FIELDS[ind] : CHOICE_FIELDS[-2]] = \
+		ballot[CHOICE_FIELDS[ind + 1] : CHOICE_FIELDS[-1]]
+	ballot[CHOICE_FIELDS[-1]] = float('nan')
+
+	return ballot
+
+
+def exhaust_beyond(ballot, ind):
+	'''
+	Replaces all choices at or after index ind with NaN, representing that the
+	ballot is exhausted at these choices
+
+	Inputs:
+		ballot (Pandas series): one ballot from the election
+		ind (int): the index to start the process at
+
+	Returns: Pandas series
+	'''
+	ballot[CHOICE_FIELDS[ind] : CHOICE_FIELDS[-1]] = float('nan')
+
+	return ballot
+
