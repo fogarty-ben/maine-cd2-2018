2d151cb0671239701b5e5bd4e479764dfbd4f0df
Ben Fogarty <ben.t.fogarty@gmail.com>
2020-02-25 12:23:56 -0600

    pull files directly from maine sos

--- get_files.sh
+++ get_files.sh
@@ -1,2 +1,3 @@
-curl -s https://www.ben-fogarty.com/projects/maine-cd2-2018/maine-cd2-2018-ballots.tar.gz > /dev/null
-tar -zxvf maine-cd2-2018-ballots.tar.gz
+#! bin/sh
+mkdir -p data
+curl 'https://www.maine.gov/sos/cec/elec/results/2018/{Nov18CVRExportFINAL1,Nov18CVRExportFINAL2,Nov18CVRExportFINAL3,RepCD2-8final,UOCAVA-FINALRepCD2,UOCAVA-AUX-CVRRepCD2,UOCAVA2CVRRepCD2,AUXCVRProofedCVR95RepCD2}.xlsx' -o "data/#1.xlsx"
